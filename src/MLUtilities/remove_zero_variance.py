6e091a185c18e95a18e28afd27e5adfa27b7e082
Nana Boateng <Nana.Boateng@niceactimize.com>
2021-09-28 22:58:39 +0100

    09-28-2021

---
+++ src/MLUtilities/remove_zero_variance.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+class remove_zero_variance:
+    def __init__(self,df):
+        self.df = df # dataframe containing the feature
+        #self.feature = feature 
+    def columns(self):
+        zero_var_cols = [col for col in self.df.columns if self.df[col].nunique(dropna=False) == 1]
+        col_keep = np.array([col for col in self.df.columns if col not in zero_var_cols])
+        return zero_var_cols
